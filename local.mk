ed38977a585c2df24b332051bdb4bbd0475f0403
Andy Yan <andyshrk@gmail.com>
2022-03-23 16:17:28 +0800

    local: Build mpv and ffmpeg from local
    Signed-off-by: Andy Yan <andyshrk@gmail.com>

--- local.mk
+++ local.mk
@@ -1,1 +1,3 @@
 WESTON_OVERRIDE_SRCDIR = /opt/WorkSpace/opensource/drm-display/weston
+FFMPEG_OVERRIDE_SRCDIR = /opt/WorkSpace/opensource/drm-display/ffmpeg
+MPV_OVERRIDE_SRCDIR = /opt/WorkSpace/opensource/drm-display/mpv
