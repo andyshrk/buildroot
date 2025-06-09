291adc71dd75d4b645df404b5600ead4fbd4b8be
Andy Yan <andyshrk@163.com>
2025-06-09 16:32:03 +0800

    configs/rk3566_box_demo: new defconfig
    Signed-off-by: Andy Yan <andyshrk@163.com>

---
+++ board/rockchip/rk3566/post-image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+BOARD_DIR="$(dirname "$0")"
+install -m 0644 -D "$BOARD_DIR/extlinux.conf" "$BINARIES_DIR/extlinux/extlinux.conf"
+support/scripts/genimage.sh -c board/rockchip/rk3566/genimage.cfg
